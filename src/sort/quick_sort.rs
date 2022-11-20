65686c92ac346dae668e5eb3485d9e2404877e5a
Teh|Striker <kristianhanus@gmail.com>
2022-11-20 14:57:13 +0100

    Quick sort fix

--- src/sort/quick_sort.rs
+++ src/sort/quick_sort.rs
@@ -1,36 +1,39 @@
 // QuickSort -
-pub fn sorting(param: Vec<i32>) {
+pub fn sorting(mut param: Vec<i32>) {
+    let length: usize = param.len() - 1;
     println!("Quick sort - input array: {:?}", param);
-    println!("Quick sort - output array: {:?}", quick_sorter(param.clone(), 0, param.len() - 1));
+    println!("Quick sort - output array: {:?}", quick_sorter(&mut param, 0, length));
 }
 
-fn quick_sorter(mut slice: Vec<i32>, low: usize, high: usize) -> Vec<i32> {
+fn quick_sorter(slice: &mut Vec<i32>, low: usize, high: usize) -> Vec<i32> {
     if low < high {;
-        let p: usize = pivot_util(slice.clone(), low, high);
-        quick_sorter(slice.clone(), low, p-1);
-        quick_sorter(slice.clone(), p+1, high);
+        let p: usize = pivot_util(slice, low, high);
+        quick_sorter(slice, low, p-1);
+        quick_sorter(slice, p+1, high);
     }
 
-    return slice;
+    return slice.to_vec();
 }
 
-fn pivot_util(mut slice: Vec<i32>, low: usize, high: usize) -> usize {
+fn pivot_util(slice: &mut Vec<i32>, low: usize, high: usize) -> usize {
     let pivot: i32 = slice[high];
     let mut  i: usize = low;
+    let mut  j: usize = low;
 
-    for j in low..high {
+    while j < high {
         if slice[j] < pivot {
             let tmp_var1: i32 = slice[i];
             let tmp_var2: i32 = slice[j];
-            slice[i] = tmp_var2;
             slice[j] = tmp_var1;
+            slice[i] = tmp_var2;
             i = i + 1;
         }
+        j = j + 1;
     }
 
     let tmp_var1: i32 = slice[i];
     let tmp_var2: i32 = slice[high];
-    slice[i] = tmp_var2;
     slice[high] = tmp_var1;
+    slice[i] = tmp_var2;
     return i;
 }
