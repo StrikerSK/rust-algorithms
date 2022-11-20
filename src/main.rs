a8aea864dc013e071cd20674f32d2554016dfe25
Teh|Striker <kristianhanus@gmail.com>
2022-11-20 22:39:43 +0100

    Linear search

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod sort;
+mod search;
 
 fn main() {
+    println!("-----------------------Sorting-----------------------");
     let input = vec![56, 123, 2, 78, 15, 79, 35, 89, 20, 54];
     sort::bubble_sort::sorting(input.clone());
     sort::merge_sort::sorting(input.clone());
     sort::quick_sort::sorting(input.clone());
+
+    println!("-----------------------Searching-----------------------");
+    search::linear_search::searching(input.clone(), 15);
 }
---
+++ src/search/linear_search.rs
@@ -0,0 +1,17 @@
+pub fn searching(input: Vec<i32>, search_value: i32) {
+    println!("Linear search - input array: {:?}", input);
+    println!("Linear search - search value: {:?}", search_value);
+
+    let mut index: usize = 0;
+    let mut i: usize = 0;
+
+    while i < input.len() {
+        if input[i] == search_value {
+            index = i;
+            break;
+        }
+        i = i + 1;
+    }
+
+    println!("Linear search - Value {:?} found at index: {:?}", search_value, index);
+}
