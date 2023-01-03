68ce9fb8334f1084be1b89338c0152172100c612
Teh|Striker <kristianhanus@gmail.com>
2023-01-03 11:50:27 +0100

    Searching interface - Exponential search

--- src/search/exponential_search.rs
+++ src/search/exponential_search.rs
@@ -1,40 +1,78 @@
+use std::borrow::Borrow;
+use crate::search::searching_trait::SearchingTrait;
+
 // This has same behaviour as binary search => input array must be sorted
 pub fn searching(input:&Vec<i32>, search_value: i32) {
     println!("Exponential search - input array: {:?}", input);
     println!("Exponential search - search value: {:?}", search_value);
 
-    let index: usize = exponential_searching(&input.clone(), input.len(),search_value);
-    println!("Exponential search - Value {:?} found at index: {:?}", search_value, index);
+    // let index: usize = exponential_searching(&input.clone(), input.len(),search_value);
+    // println!("Exponential search - Value {:?} found at index: {:?}", search_value, index);
 }
 
-fn exponential_searching(input: &Vec<i32>, n: usize, x: i32) -> usize {
+struct ExponentialSearch;
+
+impl SearchingTrait for ExponentialSearch {
+    fn search(&self, input:&Vec<i32>, search_value: i32) -> i32 {
+        exponential_searching(input.borrow(), input.len(), search_value) as i32
+    }
+}
+
+fn exponential_searching(input: &Vec<i32>, n: usize, x: i32) -> i32 {
     if input[0] == x {
-        return 0;
+        return -1;
     }
 
-    let mut bound: usize = 1;
+    let mut bound: i32 = 1;
 
-    while bound < n && input[bound] <= x {
+    while bound < n as i32 && input[bound as usize] <= x {
         bound = bound * 2;
     }
 
-    return binary_searching(input, bound / 2, bound.min((n - 1) as usize), x);
+    return binary_searching(input, bound / 2, bound.min((n - 1) as i32), x);
 }
 
-fn binary_searching(input: &Vec<i32>, low: usize, high: usize, search_value: i32) -> usize {
+fn binary_searching(input: &Vec<i32>, low: i32, high: i32, search_value: i32) -> i32 {
     if high >= low {
-        let mid: usize = low + (high - low) / 2;
+        let mid: i32 = (low + (high - low) / 2) as i32;
 
-        if input[mid] == search_value {
+        if input[mid as usize] == search_value {
             return mid;
         }
 
-        if input[mid] > search_value {
+        if input[mid as usize] > search_value {
             return binary_searching(input, low, mid - 1, search_value);
         }
 
         return binary_searching(input, mid + 1, high, search_value);
     }
 
-    return 0;
+    return -1;
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::borrow::Borrow;
+    use super::*;
+
+    #[test]
+    // Example of searching that is not working on unsorted array
+    fn test_unsorted_searching() {
+        let input: Vec<i32> = vec![56, 123, 2, 78, 15, 79, 35, 89, 20, 54];
+        assert_eq!(exponential_searching(input.borrow(), input.len(), 89), -1);
+    }
+
+    #[test]
+    fn test_sorted_searching() {
+        let input: Vec<i32> = vec![2, 15, 20, 35, 54, 56, 78, 79, 89, 123];
+        assert_eq!(exponential_searching(input.borrow(), input.len(), 89), 8);
+    }
+
+    #[test]
+    fn test_searching_nonexisting() {
+        let input: Vec<i32> = vec![2, 15, 20, 35, 54, 56, 78, 79, 89, 123];
+        assert_eq!(exponential_searching(input.borrow(), input.len(), 1000), -1);
+    }
+
 }
